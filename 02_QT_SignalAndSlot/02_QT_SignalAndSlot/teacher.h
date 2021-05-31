c0814e4db5c25411c9c9d2744605c03ce76a8749
xidaozheng <13184136638@163.com>
2021-05-31 00:16:46 -0700

    complete project content: 1. self-define button components class 2. signal and slot is overload, by using function-pointor selete any function 3. how to connect signal with other signal 4. Lambda expression 	[=](param)->int{ 		param = 0;
    		return param;
    	}

---
+++ 02_QT_SignalAndSlot/02_QT_SignalAndSlot/teacher.h
@@ -0,0 +1,16 @@
+#ifndef TEACHER_H
+#define TEACHER_H
+
+
+class Teacher : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Teacher(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // TEACHER_H
