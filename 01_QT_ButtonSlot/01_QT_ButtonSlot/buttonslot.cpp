0e12ec5b54e5c214b3db13fabf53ed278b5a76d5
xidaozheng <13184136638@163.com>
2021-05-30 19:46:05 -0700

    complete project using "OK" button ,to close dialog

--- 01_QT_ButtonSlot/01_QT_ButtonSlot/buttonslot.cpp
+++ 01_QT_ButtonSlot/01_QT_ButtonSlot/buttonslot.cpp
@@ -1,8 +1,23 @@
 #include "buttonslot.h"
+#include "QPushButton"
 
 ButtonSlot::ButtonSlot(QWidget *parent)
     : QWidget(parent)
 {
+    QPushButton *btn = new QPushButton;
+
+    setWindowTitle("ButtonSlot");
+//    resize(600, 400);
+    setFixedSize(600, 400);
+
+    btn->setParent(this);
+
+    btn->setText("OK");
+    btn->move(260, 180);
+
+    connect(btn, &QPushButton::clicked, this, &ButtonSlot::close);
+
+    btn->show();
 }
 
 ButtonSlot::~ButtonSlot()
