792189b03c54a504c62a1d715954e44ac14da20e
xidaozheng <13184136638@163.com>
2021-05-31 22:32:36 -0700

    study notes

--- 01_QT_ButtonSlot/01_QT_ButtonSlot/README.md
+++
@@ -1,1 +0,0 @@
-注意路径是：/home/xi/juniorQT/01_QT_ButtonSlot/01_QT_ButtonSlot
--- 03_QT_QMainWindow/03_QT_QMainWindow/mainwindow.cpp
+++ 03_QT_QMainWindow/03_QT_QMainWindow/mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include <QMenuBar>
 #include <QToolBar>
+#include <QStatusBar>
+#include <QLabel>
+#include <QDockWidget>
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent)
@@ -8,53 +12,86 @@ MainWindow::MainWindow(QWidget *parent) :
     resize(600, 400);
 
 /***********************************************/
-    QMenuBar *bar = menuBar();
-    setMenuBar(bar);
-
-    QMenu *fileMenu = bar->addMenu("File");
-    fileMenu->addAction("New");
-    fileMenu->addAction("Open");
-    fileMenu->addAction("Recent file");
-    fileMenu->addSeparator();
-    fileMenu->addAction("save");
-
-
-    QMenu *EditMenu = bar->addMenu("Edit");
-    EditMenu->addAction("undo");
-    EditMenu->addAction("redo");
-    fileMenu->addSeparator();
-    EditMenu->addAction("cut");
-    EditMenu->addAction("copy");
-    EditMenu->addAction("paste");
-
-
-    QMenu *BuildMenu = bar->addMenu("Build");
-    BuildMenu->addAction("build all project");
-    BuildMenu->addAction("build current project");
-    BuildMenu->addAction("qmake");
-    fileMenu->addSeparator();
-    BuildMenu->addAction("Run");
-
-    QMenu *DebugMenu = bar->addMenu("Help");
-    DebugMenu->addAction("directory");
-    DebugMenu->addAction("index");
-    fileMenu->addSeparator();
-    DebugMenu->addAction("F1");
-    DebugMenu->addAction("about QT Creator");
+/*****************==MenuBar==*******************/
 /***********************************************/
+    {
+        QMenuBar *bar = menuBar();
+        setMenuBar(bar);
 
-    QToolBar *toolBar = new QToolBar(this);
-    addToolBar(Qt::LeftToolBarArea,toolBar);
+        QMenu *fileMenu = bar->addMenu("File");
+        fileMenu->addAction("New");
+        fileMenu->addAction("Open");
+        fileMenu->addAction("Recent file");
+        fileMenu->addSeparator();
+        fileMenu->addAction("save");
 
-    toolBar->setFloatable(false);
-    toolBar->setMovable(false);
 
-    toolBar->addAction("Welcome");
-    toolBar->addAction("Edit");
-    toolBar->addAction("Design");
-    toolBar->addAction("Debug");
-    toolBar->addAction("Project");
-    toolBar->addAction("Help");
+        QMenu *EditMenu = bar->addMenu("Edit");
+        EditMenu->addAction("undo");
+        EditMenu->addAction("redo");
+        fileMenu->addSeparator();
+        EditMenu->addAction("cut");
+        EditMenu->addAction("copy");
+        EditMenu->addAction("paste");
+
+
+        QMenu *BuildMenu = bar->addMenu("Build");
+        BuildMenu->addAction("build all project");
+        BuildMenu->addAction("build current project");
+        BuildMenu->addAction("qmake");
+        fileMenu->addSeparator();
+        BuildMenu->addAction("Run");
+
+        QMenu *DebugMenu = bar->addMenu("Help");
+        DebugMenu->addAction("directory");
+        DebugMenu->addAction("index");
+        fileMenu->addSeparator();
+        DebugMenu->addAction("F1");
+        DebugMenu->addAction("about QT Creator");
+    }
+
+/***********************************************/
+/*****************==ToolBar==*******************/
+/***********************************************/
+    {
+        QToolBar *toolBar = new QToolBar(this);
+        addToolBar(Qt::LeftToolBarArea,toolBar);
+
+        toolBar->setFloatable(false);
+        toolBar->setMovable(false);
+
+        toolBar->addAction("Welcome");
+        toolBar->addAction("Edit");
+        toolBar->addAction("Design");
+        toolBar->addAction("Debug");
+        toolBar->addAction("Project");
+        toolBar->addAction("Help");
+    }
+
+/***********************************************/
+/*****************==StatusBar==*****************/
+/***********************************************/
+    {
+        QStatusBar *stBar = statusBar();
+        setStatusBar(stBar);
+
+        //place lable
+        QLabel *lable = new QLabel("left", this);
+        stBar->addWidget(lable);
+
+        //place lable
+        QLabel *lable2 = new QLabel("right", this);
+        stBar->addPermanentWidget(lable2);
+    }
+
+
+/***********************************************/
+/*****************==DockBar==*******************/
+/***********************************************/
+    {
+        QDockWidget *dwBar = new QDockWidget("float", this);
+        addDockWidget(dwBar);
+    }
 }
 
 MainWindow::~MainWindow()
