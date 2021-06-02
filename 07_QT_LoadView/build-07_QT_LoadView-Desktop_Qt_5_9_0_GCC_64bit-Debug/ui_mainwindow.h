9b61c242cb45640e7add1ffcfeef07ec6fec2af4
xidaozheng <13184136638@163.com>
2021-06-01 18:33:31 -0700

    restudy

--- 07_QT_LoadView/build-07_QT_LoadView-Desktop_Qt_5_9_0_GCC_64bit-Debug/ui_mainwindow.h
+++ 07_QT_LoadView/build-07_QT_LoadView-Desktop_Qt_5_9_0_GCC_64bit-Debug/ui_mainwindow.h
@@ -31,18 +31,17 @@ class Ui_MainWindow
 public:
     QWidget *centralWidget;
     QVBoxLayout *verticalLayout;
-    QWidget *widget;
-    QGridLayout *gridLayout;
-    QSpacerItem *horizontalSpacer_2;
-    QSpacerItem *horizontalSpacer;
-    QLineEdit *lineEdit_2;
-    QSpacerItem *horizontalSpacer_4;
-    QSpacerItem *horizontalSpacer_3;
-    QLineEdit *lineEdit;
-    QLabel *label;
-    QLabel *label_2;
-    QSpacerItem *verticalSpacer;
     QWidget *widget_2;
+    QGridLayout *gridLayout;
+    QLabel *label;
+    QLineEdit *lineEdit_2;
+    QLineEdit *lineEdit;
+    QSpacerItem *horizontalSpacer;
+    QSpacerItem *horizontalSpacer_2;
+    QLabel *label_2;
+    QSpacerItem *horizontalSpacer_3;
+    QSpacerItem *horizontalSpacer_4;
+    QWidget *widget;
     QHBoxLayout *horizontalLayout;
     QSpacerItem *horizontalSpacer_5;
     QPushButton *pushButton;
@@ -63,66 +62,62 @@ public:
         verticalLayout->setSpacing(6);
         verticalLayout->setContentsMargins(11, 11, 11, 11);
         verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
-        widget = new QWidget(centralWidget);
-        widget->setObjectName(QStringLiteral("widget"));
+        widget_2 = new QWidget(centralWidget);
+        widget_2->setObjectName(QStringLiteral("widget_2"));
         QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
         sizePolicy.setHorizontalStretch(0);
         sizePolicy.setVerticalStretch(0);
-        sizePolicy.setHeightForWidth(widget->sizePolicy().hasHeightForWidth());
-        widget->setSizePolicy(sizePolicy);
-        gridLayout = new QGridLayout(widget);
+        sizePolicy.setHeightForWidth(widget_2->sizePolicy().hasHeightForWidth());
+        widget_2->setSizePolicy(sizePolicy);
+        gridLayout = new QGridLayout(widget_2);
         gridLayout->setSpacing(6);
         gridLayout->setContentsMargins(11, 11, 11, 11);
         gridLayout->setObjectName(QStringLiteral("gridLayout"));
-        horizontalSpacer_2 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+        label = new QLabel(widget_2);
+        label->setObjectName(QStringLiteral("label"));
 
-        gridLayout->addItem(horizontalSpacer_2, 2, 0, 1, 1);
+        gridLayout->addWidget(label, 0, 1, 1, 1);
+
+        lineEdit_2 = new QLineEdit(widget_2);
+        lineEdit_2->setObjectName(QStringLiteral("lineEdit_2"));
+        lineEdit_2->setEchoMode(QLineEdit::Password);
+
+        gridLayout->addWidget(lineEdit_2, 1, 3, 1, 1);
+
+        lineEdit = new QLineEdit(widget_2);
+        lineEdit->setObjectName(QStringLiteral("lineEdit"));
+
+        gridLayout->addWidget(lineEdit, 0, 3, 1, 1);
 
         horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
 
         gridLayout->addItem(horizontalSpacer, 0, 0, 1, 1);
 
-        lineEdit_2 = new QLineEdit(widget);
-        lineEdit_2->setObjectName(QStringLiteral("lineEdit_2"));
-        lineEdit_2->setEchoMode(QLineEdit::Password);
+        horizontalSpacer_2 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
 
-        gridLayout->addWidget(lineEdit_2, 2, 2, 1, 1);
+        gridLayout->addItem(horizontalSpacer_2, 1, 0, 1, 1);
 
-        horizontalSpacer_4 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+        label_2 = new QLabel(widget_2);
+        label_2->setObjectName(QStringLiteral("label_2"));
 
-        gridLayout->addItem(horizontalSpacer_4, 2, 3, 1, 1);
+        gridLayout->addWidget(label_2, 1, 1, 1, 2);
 
         horizontalSpacer_3 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
 
-        gridLayout->addItem(horizontalSpacer_3, 0, 3, 1, 1);
+        gridLayout->addItem(horizontalSpacer_3, 0, 4, 1, 1);
 
-        lineEdit = new QLineEdit(widget);
-        lineEdit->setObjectName(QStringLiteral("lineEdit"));
+        horizontalSpacer_4 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
 
-        gridLayout->addWidget(lineEdit, 0, 2, 1, 1);
-
-        label = new QLabel(widget);
-        label->setObjectName(QStringLiteral("label"));
-
-        gridLayout->addWidget(label, 0, 1, 1, 1);
-
-        label_2 = new QLabel(widget);
-        label_2->setObjectName(QStringLiteral("label_2"));
-
-        gridLayout->addWidget(label_2, 2, 1, 1, 1);
-
-        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
-
-        gridLayout->addItem(verticalSpacer, 1, 1, 1, 1);
+        gridLayout->addItem(horizontalSpacer_4, 1, 4, 1, 1);
 
 
-        verticalLayout->addWidget(widget);
+        verticalLayout->addWidget(widget_2);
 
-        widget_2 = new QWidget(centralWidget);
-        widget_2->setObjectName(QStringLiteral("widget_2"));
-        sizePolicy.setHeightForWidth(widget_2->sizePolicy().hasHeightForWidth());
-        widget_2->setSizePolicy(sizePolicy);
-        horizontalLayout = new QHBoxLayout(widget_2);
+        widget = new QWidget(centralWidget);
+        widget->setObjectName(QStringLiteral("widget"));
+        sizePolicy.setHeightForWidth(widget->sizePolicy().hasHeightForWidth());
+        widget->setSizePolicy(sizePolicy);
+        horizontalLayout = new QHBoxLayout(widget);
         horizontalLayout->setSpacing(6);
         horizontalLayout->setContentsMargins(11, 11, 11, 11);
         horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
@@ -130,7 +125,7 @@ public:
 
         horizontalLayout->addItem(horizontalSpacer_5);
 
-        pushButton = new QPushButton(widget_2);
+        pushButton = new QPushButton(widget);
         pushButton->setObjectName(QStringLiteral("pushButton"));
 
         horizontalLayout->addWidget(pushButton);
@@ -139,7 +134,7 @@ public:
 
         horizontalLayout->addItem(horizontalSpacer_6);
 
-        pushButton_2 = new QPushButton(widget_2);
+        pushButton_2 = new QPushButton(widget);
         pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
 
         horizontalLayout->addWidget(pushButton_2);
@@ -149,7 +144,7 @@ public:
         horizontalLayout->addItem(horizontalSpacer_7);
 
 
-        verticalLayout->addWidget(widget_2);
+        verticalLayout->addWidget(widget);
 
         MainWindow->setCentralWidget(centralWidget);
 
@@ -161,8 +156,8 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
-        label->setText(QApplication::translate("MainWindow", "user:", Q_NULLPTR));
-        label_2->setText(QApplication::translate("MainWindow", "password:", Q_NULLPTR));
+        label->setText(QApplication::translate("MainWindow", "user", Q_NULLPTR));
+        label_2->setText(QApplication::translate("MainWindow", "password", Q_NULLPTR));
         pushButton->setText(QApplication::translate("MainWindow", "Enter", Q_NULLPTR));
         pushButton_2->setText(QApplication::translate("MainWindow", "Quit", Q_NULLPTR));
     } // retranslateUi
